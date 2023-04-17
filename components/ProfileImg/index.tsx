2696c39d8a23849b8dbb7ec3b92af1271c303865
Adrian Senkevich <adrianscorp@gmail.com>
2023-04-17 23:55:24 +0200

    removed useless buttons

--- components/ProfileImg/index.tsx
+++ components/ProfileImg/index.tsx
@@ -1,12 +1,12 @@
 import { app, storage, uploadFile } from '@/Firebase/config';
 import { getAuth } from 'firebase/auth';
 import { ref } from 'firebase/storage';
-import { ChangeEvent, useRef, useState } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import style from './profileimg.module.scss';
 
 const ProfileIMg = () => {
   const auth = getAuth(app);
-  const clickRef = useRef<any>(null);
+  const clickRef = useRef<HTMLInputElement | null>(null);
 
   const [url, setUrl] = useState<File | null>(null);
 
@@ -17,30 +17,38 @@ const ProfileIMg = () => {
     }
   };
 
-  const uploadImage = () => {
-    uploadFile(auth, ref(storage, url!.name), url!);
+  const uploadImage = async () => {
+    if (url) {
+      await uploadFile(auth, ref(storage, url.name), url);
+    }
+    setUrl(null);
   };
+
+  useEffect(() => {
+    if (url) {
+      uploadImage();
+    }
+  }, [url]);
 
   return (
     <>
       <div className={style.image}>
-        {auth.currentUser?.photoURL ? <img src={auth.currentUser?.photoURL}></img> : null}
+        {auth.currentUser?.photoURL ? (
+          <img
+            onClick={() => {
+              clickRef.current!.click();
+            }}
+            src={auth.currentUser?.photoURL}></img>
+        ) : null}
       </div>
       <div>
-        <button
-          onClick={() => {
-            clickRef.current.click();
-          }}>
-          Select
-        </button>
         <input
           onChange={handleFiles}
-          ref={clickRef}
+          ref={clickRef!}
           type="file"
           accept="image/*"
           className={style.files}
         />
-        <button onClick={uploadImage}>Upload!</button>
       </div>
     </>
   );
